61fc9ea4a7732595de89a1713c819d3cf53ae2f1
Marcos García <garciafmarcos@gmail.com>
2020-05-31 21:45:27 +0200

    Ejercicio 1, 2 y 3

--- src/playground.ts
+++ src/playground.ts
@@ -1,1 +1,54 @@
-console.log("This is your playground, experiment with TS code and check the console");
+const names = ["Marcos", "Yoland", "Rosa", "Julia"];
+const nationalities = ["Spanish", "English", "Russian"];
+const employee = {
+  name: "Marcos",
+  department: "UI / Frontend",
+  skills: "JS",
+};
+
+// 1. Array operations
+
+// Head
+
+const head = ([first]) => first;
+
+console.log(`f Head > ${head(names)}`);
+
+// Tail
+
+const tail = ([, ...rest]) => rest;
+
+console.log(`f Tail > ${tail(names)}`);
+
+// Init
+
+const init = (array) => array.slice(0, array.length - 1);
+
+console.log(`f Init > ${init(names)}`);
+
+// Last
+
+const last = (array) => array.slice(array.length - 1);
+
+console.log(`f Last > ${last(names)}`);
+
+// 2. Concat
+
+const concat = (a, b) => [...a, ...b];
+
+console.log(concat(names, nationalities));
+
+// Optional
+
+const concatOptional = (...rest) => rest;
+
+console.log(concatOptional(...names, ...nationalities));
+
+// 3. Clone Merge
+
+function clone(source) {
+  const newObject = { ...source };
+  return newObject;
+}
+
+console.log(clone(employee));
