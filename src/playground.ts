3aee2f0cf53a55e477785fa83e4fc91617e2ffdf
Marcos García <garciafmarcos@gmail.com>
2020-06-07 20:26:27 +0200

    Ejercicio 4

--- src/playground.ts
+++ src/playground.ts
@@ -52,7 +52,7 @@ function clone(source) {
   return { ...source };;
 }
 
-console.log('f Clone >' clone(employee));
+console.log(`f Clone >` clone(employee));
 
 // Merge 
 const a = { name: "Maria", surname: "Ibañez", country: "SPA" };
@@ -62,6 +62,31 @@ const merge = (source, target)=>{
  return {...target, ...source} 
 }
 
-console.log(merge(a, b))
+console.log(`f Merge >` merge(a, b))
 
 // 4. Read Books
+
+const books = [
+  { title: "Harry Potter y la piedra filosofal", isRead: true },
+  { title: "Canción de hielo y fuego", isRead: false },
+  { title: "Devastación", isRead: true },
+];
+
+function isBookRead(books, titleToSearch) {
+return books.some(books => books.title === titleToSearch && books.isRead)
+}
+console.log(`Libro "Devastación" >` isBookRead(books, "Devastación")); // true
+console.log(`Libro "Canción de hielo y fuego" >`  isBookRead(books, "Canción de hielo y fuego")); // false
+console.log(`Libro "Los Pilares de la Tierra" >`isBookRead(books, "Los Pilares de la Tierra")); // false
+
+// 5. Slot Machine
+
+// Propiedad contador de monedas
+
+
+// Método play(); 
+// 1. incrementa mondedas
+// 2. generar 3 valores aleatorios 
+// 3 comparar los 3 valores aleatorios, 
+//  si true "mensaje" y reiniciar contador
+//  si false "mensaje"    
